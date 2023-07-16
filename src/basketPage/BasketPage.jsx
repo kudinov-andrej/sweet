2c74b73a37c46923b85947789c54c79a51b61e1f
Andrej Kudinov <kudinov.andrej@list.ru>
2023-07-16 22:46:54 +0300

    стилизовал чекбоксы, исправил стили формы

--- src/basketPage/BasketPage.jsx
+++ src/basketPage/BasketPage.jsx
@@ -30,6 +30,7 @@ function BasketPage(props) {
 
                     <p className='basket__info'>{props.cartItems.length} макаронс /  {props.getTotalPrice()} рублей</p>
                 </div>
+                <div className='basket__buy-block'></div>
                 <ProductList
                     cartItems={props.cartItems}
                     quantity={props.quantity}
@@ -44,17 +45,17 @@ function BasketPage(props) {
                     <form className='basket__form-conteiner'>
                         <div className='basket__input-box'>
                             <div className='basket__input-conteiner'>
-                                <label for="basket__form-input_typy_name" className='basket__form-label'>Ваше имя*</label>
+                                <label htmlFor="basket__form-input_typy_name" className='basket__form-label'>Ваше имя*</label>
                                 <input type='text' className='basket__form-input basket__form-input_typy_name' id="basket__form-input_typy_name" placeholder='Укажите имя' required minLength={4} maxLength={70} />
                             </div>
                             <div className='basket__input-conteiner'>
-                                <label for="basket__form-input_typy_phone" className='basket__form-label'>Ваш телефон*</label>
+                                <label htmlFor="basket__form-input_typy_phone" className='basket__form-label'>Ваш телефон*</label>
                                 <input type='tel' className='basket__form-input basket__form-input_typy_phone' id="basket__form-input_typy_phone" placeholder='+7 (___) ___-__-__' required minLength={11} maxLength={11} />
                             </div>
                         </div>
                         <div className='basket__input-box'>
                             <div className='basket__input-conteiner' id="basket__form-input_typy_button">
-                                <label for="basket__form-input_typy_button" className='basket__form-label'>Способ доставки*</label>
+                                <label htmlFor="basket__form-input_typy_button" className='basket__form-label'>Способ доставки*</label>
                                 <button type='button' className='basket__form-button'>
                                     <img className='basket__form-button-img' src={DeliveryImg} alt='картинка машинки' />
                                     <div className='basket__form-button-info'>
@@ -72,12 +73,12 @@ function BasketPage(props) {
                             </button>
                         </div>
                         <div className='basket__input-conteiner'>
-                            <label for="basket__form-input_typy_address" className='basket__form-label'>Адрес доставки</label>
+                            <label htmlFor="basket__form-input_typy_address" className='basket__form-label'>Адрес доставки</label>
                             <textarea rows="3" className='basket__form-input basket__form-input_typy_address' id="basket__form-input_typy_address" placeholder='Не нужно заполнять при самовывозе' required minLength={20} maxLength={180} />
                         </div>
                         <div className='basket__input-box'>
                             <div className='basket__input-conteiner'>
-                                <label for="basket__form-input_typy_date" className='basket__form-label'>Дата получения</label>
+                                <label htmlFor="basket__form-input_typy_date" className='basket__form-label'>Дата получения</label>
                                 <DatePicker className='basket__form-input basket__form-input_typy_date' id="basket__form-input_typy_date" placeholder='Выберите дату'>
                                     <select defaultValue={currentDate} className='basket__form-input basket__form-input_typy_date' >
                                         {dates.map(d => <Option key={d}>{d.toLocaleDateString()}</Option>)}
@@ -85,7 +86,7 @@ function BasketPage(props) {
                                 </DatePicker>
                             </div>
                             <div className='basket__input-conteiner'>
-                                <label for="basket__form-input_typy_time" className='basket__form-label'>Время</label>
+                                <label htmlFor="basket__form-input_typy_time" className='basket__form-label'>Время</label>
                                 <select className='basket__form-input    basket__form-input_typy_time' id="basket__form-input_typy_time">
                                     <option className='basket__form-input_option'>09:00-11:00</option>
                                     <option className='basket__form-input_option'>11:00-13:00</option>
@@ -97,21 +98,21 @@ function BasketPage(props) {
                             </div>
                         </div>
                         <div className='basket__input-conteiner'>
-                            <label for="basket__form-input_typy_comments" className='basket__form-label'>коментарии</label>
+                            <label htmlFor="basket__form-input_typy_comments" className='basket__form-label'>коментарии</label>
                             <textarea rows="5" className='basket__form-input basket__form-input_typy_comments' id="basket__form-input_typy_comments" placeholder='Здесь Вы можете написать пожелания, относительно анонимной доставки, текста открытки и другое.' required minLength={20} maxLength={180} />
                         </div>
                         <div className='basket__input-box'>
                             <div className='basket__checkbox-conteiner'>
-                                <label for="basket__form-input_typy_checkbox" className='basket__form-checkbox'>Оплата картой онлайн</label>
-                                <input type='checkbox' className='basket__form-checkbox basket__form-checkbox_typy_name' id="basket__form-checkbox_typy_name" required />
+                                <input type='checkbox' className='basket__form-checkbox basket__form-checkbox_typy_name' id="basket__form-input_typy_pay" required />
+                                <label htmlFor="basket__form-input_typy_pay" className='basket__form-checkbox-label'>Оплата картой онлайн</label>
                             </div>
                             <div className='basket__checkbox-conteiner'>
-                                <label for="basket__form-checkbox_typy_phone" className='basket__form-checkbox'>Наличными при получении</label>
-                                <input type='checkbox' className='basket__form-checkbox ' id="basket__form-checkbox_typy_phone" required />
+                                <input type='checkbox' className='basket__form-checkbox' id="basket__form-checkbox_typy_cash" required />
+                                <label htmlFor="basket__form-checkbox_typy_cash" className='basket__form-checkbox-label'>Наличными при получении</label>
                             </div>
                             <div className='basket__checkbox-conteiner'>
-                                <label for="basket__form-checkbox_typy_phone" className='basket__form-checkbox'>Яндекс деньги</label>
-                                <input type='checkbox' className='basket__form-checkbox' id="basket__form-checkbox_typy_phone" required />
+                                <input type='checkbox' className='basket__form-checkbox' id="basket__form-checkbox_typy_yandex" required />
+                                <label htmlFor="basket__form-checkbox_typy_yandex" className='basket__form-checkbox-label'>Яндекс деньги</label>
                             </div>
                         </div>
                     </form>
